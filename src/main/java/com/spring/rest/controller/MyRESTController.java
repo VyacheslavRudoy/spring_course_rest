c3671608d18214d0c3983a792e9ecca55f0f8768
Vyacheslav <social_smm@bk.ru>
2023-07-09 00:38:38 +0300

    add exception handling

--- src/main/java/com/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,13 @@
 package com.spring.rest.controller;
 
 import com.spring.rest.entity.Employee;
+import com.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +27,10 @@ public class MyRESTController {
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in Database");
+        }
         return employee;
     }
 }
